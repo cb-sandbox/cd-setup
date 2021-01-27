153a5434781b040dd42f7eb46fde7a774350e344
Logan Donley <ldonley@cloudbees.com>
2021-01-27 12:33:56 -0500

    Updating to use if blocks and adding nexus config

--- cd-setup.sh
+++ cd-setup.sh
@@ -9,7 +9,6 @@
 # [] Set /server/hostName when CEV-27062 is resolved
 # [x] Open ACLs for /server Everyone
 
-
 #TOMCAT_MYSQL_PW=LM9pB9Jz
 TOMCAT_MYSQL_PW=flow
 
@@ -25,26 +24,36 @@ ectool evalDsl "aclEntry principalType: 'group', principalName: 'Everyone', obje
 echo "Set CD repository hostName"
 ectool setProperty /repositories/Default/url "https://cd.${BASE_DOMAIN}:8200"
 
-echo "Adding Tomcat/MySQL QA resource"
-ectool createResource tomcat_mysql_qa --hostName $CD_AGENT_TOMCAT_QA_IP
-echo "Adding Tomcat/MySQL UAT resource"
-ectool createResource tomcat_mysql_uat --hostName $CD_AGENT_TOMCAT_UAT_IP
-
 echo "Creating EC-Kubernetes configuration"
 configurations/EC-Kubernetes/CreateConfiguration.sh "$CLUSTER_ENDPOINT" "$CLUSTER_NAME"
 
-# echo "Installing EC-Tomcat"
-ectool installPlugin http://downloads.electric-cloud.com/plugins/EC-Tomcat/2.3.6.2020103102/EC-Tomcat.jar
-ectool promotePlugin EC-Tomcat-2.3.6.2020103102 --promoted 1
-echo "Creating tomcat_qa configuration"
-configurations/EC-Tomcat/CreateConfiguration.sh tomcat_qa $TOMCAT_MYSQL_PW "http://${CD_AGENT_TOMCAT_QA_IP}:8080"
-echo "Creating tomcat_uat configuration"
-configurations/EC-Tomcat/CreateConfiguration.sh tomcat_uat $TOMCAT_MYSQL_PW "http://${CD_AGENT_TOMCAT_UAT_IP}:8080"
-echo "Creating mysql_qa configuration"
+if [ "$TF_VAR_agent_enabled" ]; then
+  echo "Adding Tomcat/MySQL QA resource"
+  ectool createResource tomcat_mysql_qa --hostName $CD_AGENT_TOMCAT_QA_IP
+  echo "Adding Tomcat/MySQL UAT resource"
+  ectool createResource tomcat_mysql_uat --hostName $CD_AGENT_TOMCAT_UAT_IP
 
-# echo "Installing EC-MYSQL"
-ectool installPlugin http://downloads.electric-cloud.com/plugins/EC-MYSQL/2.0.13.2020102201/EC-MYSQL.jar
-ectool promotePlugin EC-MYSQL-2.0.13.2020102201 --promoted 1
-configurations/EC-MYSQL/CreateConfiguration.sh mysql_qa $TOMCAT_MYSQL_PW
-echo "Creating mysql_qa configuration"
-configurations/EC-MYSQL/CreateConfiguration.sh mysql_uat $TOMCAT_MYSQL_PW
+  echo "Installing EC-Tomcat"
+  ectool installPlugin http://downloads.electric-cloud.com/plugins/EC-Tomcat/2.3.6.2020103102/EC-Tomcat.jar
+  ectool promotePlugin EC-Tomcat-2.3.6.2020103102 --promoted 1
+  echo "Creating tomcat_qa configuration"
+  configurations/EC-Tomcat/CreateConfiguration.sh tomcat_qa $TOMCAT_MYSQL_PW "http://${CD_AGENT_TOMCAT_QA_IP}:8080"
+  echo "Creating tomcat_uat configuration"
+  configurations/EC-Tomcat/CreateConfiguration.sh tomcat_uat $TOMCAT_MYSQL_PW "http://${CD_AGENT_TOMCAT_UAT_IP}:8080"
+  echo "Creating mysql_qa configuration"
+
+  echo "Installing EC-MYSQL"
+  ectool installPlugin http://downloads.electric-cloud.com/plugins/EC-MYSQL/2.0.13.2020102201/EC-MYSQL.jar
+  ectool promotePlugin EC-MYSQL-2.0.13.2020102201 --promoted 1
+  configurations/EC-MYSQL/CreateConfiguration.sh mysql_qa $TOMCAT_MYSQL_PW
+  echo "Creating mysql_qa configuration"
+  configurations/EC-MYSQL/CreateConfiguration.sh mysql_uat $TOMCAT_MYSQL_PW
+fi
+
+if [ "$NEXUS_ENABLED" ]; then
+  echo "Setting up Nexus"
+  ectool installPlugin https://downloads.electric-cloud.com/plugins/EC-Nexus/1.1.1.2020123004/EC-Nexus.jar
+  ectool promotePlugin EC-Nexus-1.1.1.2020123004 --promoted 1
+  echo "Creating nexus configuration"
+  configurations/EC-Nexus/CreateConfiguration.sh nexus "$NEXUS_TOKEN" https://nexus."$BASE_DOMAIN"
+fi
---
+++ configurations/EC-Nexus/CreateConfiguration.sh
@@ -0,0 +1,16 @@
+# Create EC-Nexus configuration
+# TODO:
+# - use IP instead of localhost
+
+NEXUS_CONFIG=$1
+NEXUS_PW=$2
+NEXUS_URL=$3
+NEXUS_USER="admin"
+
+printf "${NEXUS_PW}\n" | ectool runProcedure /plugins/EC-Nexus/project \
+  --procedureName CreateConfiguration \
+  --actualParameter \
+  instance="${NEXUS_URL}" \
+  config="${NEXUS_CONFIG}" \
+  credential="${NEXUS_CONFIG}" \
+  --credential "${NEXUS_CONFIG}"="${NEXUS_USER}"
