2725e0a728dc6d424e2e44d7e709fe7ea719c6aa
Greg Maxey <gregorymaxey@yahoo.com>
2021-02-01 17:54:30 +0000

    Add EC-Helm and configuration

--- configurations/EC-Helm/CreateConfiguration.sh
+++ configurations/EC-Helm/CreateConfiguration.sh
@@ -6,13 +6,13 @@ ectool runProcedure /plugins/EC-Helm/project \
   --procedureName CreateConfiguration \
   --actualParameter \
     config="${HELM_CONFIG}" \
+    helmPath=helm \
+    helmVersion=3 \
     checkConnection=0 \
-    checkConnectionResource= 0 \
+    checkConnectionResource=0 \
     createKubeconfig=0 \
     debugLevel=0 \
     desc="" \
-    helmPath=helm \
-    helmVersion=3 \
     kubeconfigContent="" \
     kubectlPath=kubectl \
     options=""
