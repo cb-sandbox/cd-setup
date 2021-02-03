49c71d29d2e9339e62ef2801892030f740cf46d4
Greg Maxey <gregorymaxey@yahoo.com>
2021-02-03 14:29:48 +0000

    If clause for CD server

--- cd-setup.sh
+++ cd-setup.sh
@@ -11,21 +11,36 @@
 
 TOMCAT_MYSQL_PW=flow
 
-echo "Logging into cd.${BASE_DOMAIN}"
-ectool --server "cd.${BASE_DOMAIN}" login admin "$CD_ADMIN_PASS"
+if [ true ]; then
+	echo "CloudBees CD server setup"
+	
+	echo "Logging into cd.${BASE_DOMAIN}"
+	ectool --server "cd.${BASE_DOMAIN}" login admin "$CD_ADMIN_PASS"
 
-echo "Opening up privileges for all users"
-ectool evalDsl "aclEntry principalType: 'group', principalName: 'Everyone', objectType: 'server', systemObjectName: 'server', readPrivilege: 'allow',modifyPrivilege: 'allow', changePermissionsPrivilege: 'allow', executePrivilege: 'allow'"
+	echo "Opening up privileges for all users"
+	ectool evalDsl "aclEntry principalType: 'group', principalName: 'Everyone', objectType: 'server', systemObjectName: 'server', readPrivilege: 'allow',modifyPrivilege: 'allow', changePermissionsPrivilege: 'allow', executePrivilege: 'allow'"
 
-echo "Set CD server hostname"
-ectool setProperty /server/settings/ipAddress "cd.${BASE_DOMAIN}"
-echo "Set CD repository URL"
-ectool modifyRepository default --url "https://cd.${BASE_DOMAIN}:8200"
+	echo "Set CD server hostname"
+	ectool setProperty /server/settings/ipAddress "cd.${BASE_DOMAIN}"
+	echo "Set CD repository URL"
+	ectool modifyRepository default --url "https://cd.${BASE_DOMAIN}:8200"
 
-echo "Creating EC-Kubernetes configuration"
-configurations/EC-Kubernetes/CreateConfiguration.sh "$CLUSTER_ENDPOINT" "$CLUSTER_NAME"
+	echo "Installing EC-Helm"
+	ectool installPlugin http://downloads.electric-cloud.com/plugins/EC-Helm/1.1.1.2020101501/EC-Helm.jar
+	ectool promotePlugin EC-Helm-1.1.1.2020101501 --promoted 1
+	echo "Creating EC-Helm configuration"
+	configurations/EC-Helm/CreateConfiguration.sh Helm
+
+	ectool evalDsl --dslFile "dsl/Helm Deploy/Helm.groovy"
+	ectool evalDsl --dslFile "dsl/Helm Deploy/HelmAppEnvModels.groovy"  --parameters "{\"base_domain\":\"${BASE_DOMAIN}\"}"
+
+	echo "Creating EC-Kubernetes configuration"
+	configurations/EC-Kubernetes/CreateConfiguration.sh "$CLUSTER_ENDPOINT" "$CLUSTER_NAME"
+fi
 
 if [ "$TF_VAR_agent_enabled" ]; then
+  echo "CloudBees CD VM agent setup"
+
   echo "Adding Tomcat/MySQL QA resource"
   ectool createResource tomcat_mysql_qa --hostName $CD_AGENT_TOMCAT_QA_IP
   echo "Adding Tomcat/MySQL UAT resource"
@@ -47,14 +62,6 @@ if [ "$TF_VAR_agent_enabled" ]; then
   echo "Creating mysql_uat configuration"
   configurations/EC-MYSQL/CreateConfiguration.sh mysql_uat $TOMCAT_MYSQL_PW
   
-  echo "Installing EC-Helm"
-  ectool installPlugin http://downloads.electric-cloud.com/plugins/EC-Helm/1.1.1.2020101501/EC-Helm.jar
-  ectool promotePlugin EC-Helm-1.1.1.2020101501 --promoted 1
-  echo "Creating EC-Helm configuration"
-  configurations/EC-Helm/CreateConfiguration.sh Helm
-  
-  ectool evalDsl --dslFile "dsl/Helm Deploy/Helm.groovy"
-  ectool evalDsl --dslFile "dsl/Helm Deploy/HelmAppEnvModels.groovy"  --parameters "{\"base_domain\":\"${BASE_DOMAIN}\"}"
 fi
 
 if [ "$NEXUS_ENABLED" ]; then
